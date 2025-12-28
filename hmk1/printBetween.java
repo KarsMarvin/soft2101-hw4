a6b7053f034beb6b8e341df801d4b84f13f69316
Marvin Karera <kareramarvin14@gmail.com>
2025-12-28 04:13:57 +0300

    satisfying requirement one

---
+++ hmk1/printBetween.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class printBetween {
+
+    public static void printRange(int a, int b) {
+        int start;
+        int end;
+
+        if (a < b) {
+            start = a;
+            end = b;
+        } else {
+            start = b;
+            end = a;
+        }
+
+        for (int i = start + 1; i < end; i++) {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int first = scanner.nextInt();
+
+        System.out.print("Enter second number: ");
+        int second = scanner.nextInt();
+
+        printRange(first, second);
+    }
+}
