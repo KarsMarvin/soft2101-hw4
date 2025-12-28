cc400794320c27863aacdf9923ca90ad5b4859fb
Marvin Karera <kareramarvin14@gmail.com>
2025-12-28 04:35:05 +0300

    satisfying requirement change three

--- hmk1/printBetween.java
+++ hmk1/printBetween.java
@@ -15,7 +15,9 @@ public class printBetween {
         }
 
         for (int i = start + 1; i < end; i++) {
-            System.out.println(i);
+		if(i%2==0){
+			System.out.println(i);
+		}            
         }
     }
 
